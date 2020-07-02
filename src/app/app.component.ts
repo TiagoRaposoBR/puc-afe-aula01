87824ada4211813ac1d1aedd199532275646bb79
Tiago Raposo <tiago.o.raposo@gmail.com>
2020-07-01 22:35:52 -0300

    CSS do feed concluido

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./cabecalho.app.component.css']
+  styleUrls: ['./cabecalho.app.component.css', './feed.app.component.css']
 })
 export class AppComponent {
   title = 'puc-afe-aula01';
